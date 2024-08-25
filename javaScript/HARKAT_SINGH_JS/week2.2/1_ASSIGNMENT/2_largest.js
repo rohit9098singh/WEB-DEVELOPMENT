4367d25b62c67c2950332d53afd2b0260528ccf1
rohit@ROHIT <rohitsingh95724@gmail.com>
2024-08-26 02:52:29 +0530

    2nd Assignment

---
+++ javaScript/HARKAT_SINGH_JS/week2.2/1_ASSIGNMENT/2_largest.js
@@ -0,0 +1,24 @@
+const prompt = require("prompt-sync")();
+function maxArraysize(){
+    const size = parseInt(prompt("Enter the size of an array: "));
+
+let myArray = [];
+
+for (let i = 0; i < size; i++) {
+    myArray[i] = parseInt(prompt(`Enter the element at index ${i + 1}: `));
+}
+
+console.log("The array elements are:", myArray);
+
+let maxEement=myArray[0];
+
+for(let i=1 ;i<=myArray.length;i++){
+    if(myArray[i]>maxEement){
+        maxEement=myArray[i];
+    }
+}
+console.log("the maximum element from your array is = ",maxEement);
+
+}
+
+maxArraysize();
