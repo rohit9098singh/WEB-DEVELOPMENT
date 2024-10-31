9d1b23f9024643f31deb429ee167393f7b1d6c14
rohit@ROHIT <rohitsingh95724@gmail.com>
2024-10-31 09:57:27 +0530

    gg

---
+++ javaScript/HARKAT_SINGH_JS/week7.1/Week7.2_context/src/App.jsx
@@ -0,0 +1,89 @@
+    // // App.jsx
+    // import { useContext, useState } from 'react';
+    // import './App.css';
+    // import { CountContext } from './context'; // Import the context from context.js
+
+    // function App() {
+    //   const [count, setCount] = useState(0);
+
+    //   return (
+    //     <CountContext.Provider value={count}>
+    //       <Count setCount={setCount} />
+    //     </CountContext.Provider>
+    //   );
+    // }
+
+    // export default App;
+
+    // // Count component that renders CountRender and Button components
+    // function Count({ setCount }) {
+    //   return (
+    //     <div>
+    //       <CountRender />
+    //       <Button setCount={setCount} />
+    //     </div>
+    //   );
+    // }
+
+    // // Component to render the count value
+    // function CountRender() {
+    //   const count = useContext(CountContext); // Access the count from CountContext
+    //   return <div>{count}</div>;
+    // }
+
+    // // Component for buttons to increase or decrease the count
+    // function Button({ setCount }) {
+    //   const count = useContext(CountContext); // Access the count from CountContext
+    //   return (
+    //     <div>
+    //       <button onClick={() => setCount(count + 1)}>Increase</button>
+    //       <button onClick={() => setCount(count - 1)}>Decrease</button>
+    //     </div>
+    //   );
+    // }
+
+//=========================================== USING RECOIL METHOD ====================================================
+
+import { RecoilRoot } from "recoil";
+
+function App(){
+  return(
+    <div>
+       <RecoilRoot>
+         <Count/>
+       </RecoilRoot>
+    </div>
+  )
+}
+
+function Count(){
+  return (
+    <div>
+       <CountRenderer/>
+       <Buttons/>
+    </div>
+  )
+}
+
+function countRenderer(){
+  const count=useRecoilValue(countAtom); // because here only i need the value not the full state  const [count,setCOunt]=useRecoilState(countAtom);
+  return(
+    <div>
+       <b>
+         {count}
+       </b>
+    </div>
+  )
+}
+
+function Button(){
+  const [count,setCount]=useRecoilState(countAtom);
+    return(
+      <div>
+              <button onClick={() => setCount(count + 1)}>Increase</button>
+              <button onClick={() => setCount(count - 1)}>Decrease</button>
+      </div>
+    )
+}
+
+export default App;
---
+++ javaScript/HARKAT_SINGH_JS/week7.1/Week7.2_context/src/Context.js
@@ -0,0 +1,5 @@
+// context.js
+import { createContext } from "react";
+
+// Create and export CountContext with an initial value of 0
+export const CountContext = createContext(0);
